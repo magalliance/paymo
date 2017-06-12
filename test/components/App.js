a38836c8d75bc9e5fc1b63542f9a21d66040f375
Anton Byaltsev <magalliance@gmail.com>
2017-06-12 16:58:33 +0300

    Fix import module in test component

--- test/components/App.js
+++ test/components/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { shallow } from "enzyme";
 import assert from "assert";
-import { App } from "../../src/components/App";
+import App from "../../src/components";
 
 // unit tests for the App component
 describe('App component', () => {
   describe('render()', () => {
     it('should render the component', () => {
-      const props = {dispatch: ()=>{}, users: []};
-      const wrapper = shallow(<App {...props}/>);
+      const wrapper = shallow(<App />);
       assert.equal(wrapper.length, 1);
     });
   });
